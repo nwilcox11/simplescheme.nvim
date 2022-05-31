4a4167c2d14cad2dddb13f8c8cdcc310a25779ad
nwilcox11 <wilcox.nicholas.m@gmail.com>
2022-05-30 22:30:38 -0400

    Color shifts

--- lua/simplescheme.lua
+++ lua/simplescheme.lua
@@ -2,19 +2,33 @@ local Color, c, Group, g, s = require('colorbuddy').setup()
 
 vim.g.colors_name = 'simplescheme'
 
-Color.new('blue',      '#81a2be')
-Color.new('white',     '#f2e5bc')
-Color.new('red',       '#cc6666')
-Color.new('orange',    '#de935f')
-Color.new('green',     '#99cc99')
+Color.new('dark_blue', '#2e3440')
+Color.new('white', '#Eceff4')
+Color.new('snow', '#D8Dee9')
+Color.new('light_snow', '#e5e9f0')
+Color.new('blue', '#5e81ac')
+Color.new('light_blue', '#81a1c1')
+Color.new('blue_green', '#88c0d0')
+Color.new('turquoise', '#8fbcbb') -- types
+
+Group.new('blue07', c.turquoise)
+
+Color.new('red', '#bf616a')
+Color.new('orange', '#d08770')
+Color.new('yellow',    '#ebcb8b')
+Color.new('purple', '#b48ead')
+Color.new('green',     '#a3be8c')
+
 Color.new('fruit', '#d787af')
 Color.new('brown',     '#a3685a')
-Color.new('yellow',    '#f8fe7a')
 Color.new('seagreen',  '#698b69')
-Color.new('turquoise', '#698b69')
+
 Color.new('grey', '#949494')
 Color.new('dark', '#767676')
 Color.new('darker', '#585858')
+
+-- local hl = "highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp
+-- vim.cmd(hl)
 
 -- Info
 Group.new('Error', c.red, c.none, s.italic)
@@ -28,8 +42,10 @@ Group.new('Noise', c.dark, c.none, s.none)
 Group.new('Comment', c.dark, c.none, s.italic)
 Group.new('String', c.green, c.none, s.none)
 Group.new('MethodCall', c.grey, c.none, s.none)
-Group.new('Keyword', c.blue, c.none, s.none)
-Group.new('TypeLabel', c.red, c.none, s.italic)
+Group.new('Keyword', c.light_blue, c.none, s.none)
+Group.new('TypeLabel', c.turquoise, c.none, s.italic)
+Group.new('Methods', c.turquoise)
+Group.new('Execptions', c.purple)
 
 -- Preferred Highlight Groups -- :h group-name
 Group.link('Special', g.Normal)
@@ -41,7 +57,7 @@ Group.link('Error', g.Error)
 
 -- Minor Highlight Groups --
 Group.link('Function', g.Normal)
-Group.link('Exception', g.Error)
+Group.link('Exception', g.Execptions)
 Group.link('Keyword', g.Keyword)
 Group.link('Include', g.Keyword)
 Group.link('Delimiter', g.Misc)
@@ -52,7 +68,13 @@ Group.link('TSVariableBuiltin', g.Keyword)
 Group.link('TSTag', g.Keyword)
 Group.link('TSTagDelimiter', g.Noise)
 Group.link('TSText', g.Normal)
+Group.link('TSNumber', g.Execptions)
 Group.link('TSInclude', g.Keyword)
+Group.link('TSMethod', g.Methods)
+Group.link('TSKeywordOperator', g.Methods)
+Group.link('TSRepeat', g.Keyword)
+Group.link('TSPunctBracket', g.TypeLabel)
+Group.link('TSKeywordReturn', g.Execptions)
 
 -- Editor Groups
 Group.new('IncSearch', c.none, c.none, s.reverse)
@@ -61,4 +83,4 @@ Group.link('cssAttr', g.Normal)
 -- Diagnostics
 Group.link('DiagnosticError', g.Error)
 Group.link('DiagnosticHint', g.Misc)
-Group.link('DiagnosticWarn', g.Misc)
+Group.link('DiagnosticWarn', g.Warning)
